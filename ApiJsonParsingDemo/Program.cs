67ea982fff5464efa51c7c4740bab7780c599717
christoffer.fjellstrom <christoffer.fjellstrom@qlok.se>
2023-12-20 16:01:54 +0100

    Add project files.

---
+++ ApiJsonParsingDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace ApiJsonParsingDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "https://nameday.abalin.net/api/V1/today?timezone=Europe/Stockholm";
+
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    NamedayWrapper? result = JsonSerializer.Deserialize<NamedayWrapper>(jsonResponse);
+
+                    await Console.Out.WriteLineAsync($"Namedays for {result.Day}/{result.Month}");
+                    foreach (var pair in result.Nameday)
+                    {
+                        await Console.Out.WriteLineAsync($"{pair.Key}: {pair.Value}");
+                    }
+                } 
+                else
+                {
+                    await Console.Out.WriteLineAsync($"Failed response code: {response.StatusCode} {response.ReasonPhrase}");
+                }
+            }
+        }
+    }
+}
